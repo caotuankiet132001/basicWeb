c826ab1b3391305651236d7efbee32a0abea7332
Cao Tuấn Kiệt <caotuankiet132001@Gmail.com>
2022-10-06 18:54:23 +0700

    PustProject

---
+++ HTML/javascript.js
@@ -0,0 +1,26 @@
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var swipes = document.getElementsByClassName("swipe");
+    var dots =document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < swipes.length; i++) {
+        swipes[i].className = swipes[i].className.replace(" active", "");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    swipes[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " active";
+}
